af45718cc6da401723d9b9f2b9d83cd7207cc4b4
Tiago Abreu <tabreu@NONI.mirabeau.nl>
2018-12-11 16:28:51 +0100

    Day 1 Challenge 2

--- Day1/Day1.fs
+++ Day1/Day1.fs
@@ -1,6 +1,25 @@
 ﻿module Day1
 open CommonLib.Files
 
+type Leaf = {value:int; left:Leaf option; right:Leaf option}
+
+let rec insertLeaf (tree:Leaf option) (newValue:int) =
+    match tree with 
+    | Some t -> if newValue < t.value then {t with left=Some (insertLeaf t.left newValue)}
+                else if newValue > t.value then {t with right=Some (insertLeaf t.right newValue)}
+                else t
+    | None -> {value=newValue; left=None; right=None}
+
+let rec searchForLeaf (tree:Leaf option) (searchFor:int) : Leaf option = 
+    match tree with 
+    | Some t -> if searchFor = t.value then Some t
+                else if searchFor < t.value then searchForLeaf t.left searchFor
+                else if searchFor > t.value then searchForLeaf t.right searchFor
+                else None
+    | None -> None
+
+// Credit to https://lukemerrett.com/fsharp-binary-search-tree/
+
 let getIntFromStringWithSignal s =
     System.Int32.Parse(s, System.Globalization.NumberStyles.AllowLeadingSign)
 
@@ -14,3 +33,17 @@ let getIntValuesFromFile fileName =
 let getInitialResultingFrequency (fileName:string) (initialFrequency:int) =
     getIntValuesFromFile fileName
     |> Array.fold getNewFrequency initialFrequency
+
+let getFirstDuplicateFrequency (fileName:string) (initialFrequency:int) =
+    let values = getIntValuesFromFile fileName
+    let mutable currentFrequency = initialFrequency
+    let mutable tree = Some { value=0; left=None; right=None}
+    let mutable doubleFrequency = 0
+    while (doubleFrequency = 0) do
+        for i in values do
+            currentFrequency <- getNewFrequency currentFrequency i
+            let duplicateValue = searchForLeaf tree currentFrequency
+            if (duplicateValue <> None && doubleFrequency = 0)
+            then doubleFrequency <- currentFrequency
+            else tree <- Some (insertLeaf tree currentFrequency)
+    doubleFrequency
--- Day1/Program.fs
+++ Day1/Program.fs
@@ -8,6 +8,8 @@ let main argv =
     printfn "Hello World from F#!"
     let result = getInitialResultingFrequency "input.txt" 0
     printfn "%A" (result)
+    let result = getFirstDuplicateFrequency "input.txt" 0
+    printfn "%A" (result)
     let text = Console.ReadLine()
     0
     0 // return an integer exit code

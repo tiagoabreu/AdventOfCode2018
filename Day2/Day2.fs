8043d0c393a39b1120514df3fd484f7673e0bc4b
Tiago Abreu <tabreu@tabreu.com>
2018-12-12 13:18:49 +0100

    Day 2 Challenge 1 Also, some cleanup

---
+++ Day2/Day2.fs
@@ -0,0 +1,42 @@
+﻿module Day2
+
+open CommonLib.Files
+
+let arrayHasElementRepeated arr numOfRepetitions =
+    let getOccurencesPerLetter str =
+        Array.countBy (fun elem -> elem) str
+
+    let isCorrectNumberOfOccurences (occurenceElem : 'a * int) =
+        snd occurenceElem = numOfRepetitions
+
+    getOccurencesPerLetter arr
+    |> Array.tryFind isCorrectNumberOfOccurences
+    |> Option.isSome
+
+let calculateCheckSum (arr:int[]) =
+    Array.fold (fun acc cur -> acc * cur) 1 arr
+
+let incrementCountInCheckSum (checkSumValues:(int * int)[]) valueToIncrement =
+    let index = Array.findIndex (fun x -> fst x = valueToIncrement) checkSumValues
+    let tupletoChange = checkSumValues.[index]
+    Array.set checkSumValues index (valueToIncrement, snd tupletoChange + 1)
+
+let getCheckSumOfIds (repetitionCheck:int[]) (idArr:string[]) =
+    let checkSumValues : (int * int)[] = Array.map (fun x -> (x, 0)) repetitionCheck
+
+    let processRepetitions str =
+        let processRepetition charArr repetition  =
+            if (arrayHasElementRepeated charArr repetition) then
+                    incrementCountInCheckSum checkSumValues repetition
+
+        let charArr = explode str
+        Array.map (processRepetition charArr) repetitionCheck |> ignore
+
+    Array.map processRepetitions idArr |> ignore
+
+    Array.map (fun x -> snd x) checkSumValues
+    |> calculateCheckSum
+
+let getCheckSumOfFile fileName (repetitionCheck:int[]) =
+    getLineValuesFromFilePath fileName
+    |> getCheckSumOfIds repetitionCheck
---
+++ Day2/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day2
+
+[<EntryPoint>]
+let main argv =
+    let result = getCheckSumOfFile "input.txt"  [|2 .. 3|]
+    printfn "%A" (result)
+    let text = Console.ReadLine()
+    0 // return an integer exit code

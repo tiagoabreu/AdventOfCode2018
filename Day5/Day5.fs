a6154b4cc7567c3c245ccd816ae0c9c6208227dc
Tiago Abreu <tabreu@tabreu.com>
2018-12-14 14:16:13 +0100

    Day 5 Challenges 1 and 2

---
+++ Day5/Day5.fs
@@ -0,0 +1,44 @@
+﻿module Day5
+
+open System
+open CommonLib.Files
+
+let triggerReaction (chain:char[]) =
+    let mutable newChain = chain
+    let isCharToRemove idx chr = 
+            chain.Length > idx + 1 
+            && chain.[idx] <> '-' 
+            && chr <> chain.[idx+1]
+            && (Char.ToLower chr) = (Char.ToLower chain.[idx+1])
+
+    for idx,chr in Array.indexed chain do
+        if (isCharToRemove idx chr) then
+            Array.set newChain idx '-'
+            Array.set newChain (idx+1) '-'
+    Array.filter (fun x -> x <> '-') newChain
+
+let rec getLengthOfFinalReaction currentLength (chain:char[]) =
+    let reactionChain = triggerReaction chain
+    if (currentLength = reactionChain.Length) then
+        reactionChain.Length
+    else getLengthOfFinalReaction reactionChain.Length reactionChain
+
+let getMinimalReaction (chain:char[]) =
+    let mutable minimalReactionLength = Int32.MaxValue
+    let charArr = [|'a'..'z'|]
+    for i in charArr do
+        let currentReactionLength = getLengthOfFinalReaction Int32.MaxValue
+                                    <| Array.filter (fun x -> (Char.ToLower x) <> i) chain
+        if (currentReactionLength < minimalReactionLength) then minimalReactionLength <- currentReactionLength
+    minimalReactionLength
+
+let getLengthFromReactionInFile fileName =
+    (getLineValuesFromFilePath fileName).[0]
+    |> explode 
+    |> getLengthOfFinalReaction Int32.MaxValue 
+
+let getMinimalReactionInFile fileName =
+    (getLineValuesFromFilePath fileName).[0]
+    |> explode 
+    |> getMinimalReaction
+
---
+++ Day5/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Day5
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let result = getLengthFromReactionInFile "input.txt"
+    printfn "%A" (result)
+    let result = getMinimalReactionInFile "input.txt"
+    printfn "%A" (result)
+    let text = Console.ReadLine()
+    0 // return an integer exit code
